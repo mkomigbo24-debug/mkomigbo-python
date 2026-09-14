a444cadeb142a0b50d25f6b3ef23f779e51bbb15
SkillsContinua <you@skillscontinua.com>
2026-09-14 14:22:52 +0100

    Fix: Rename lang2 -> language2 (Python conflict) + Tone thesis akwa 3-way proof loaded

--- language2/apps.py
+++ language2/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class Lang2Config(AppConfig):
+class Language2Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lang2'
+    name = 'language2'
