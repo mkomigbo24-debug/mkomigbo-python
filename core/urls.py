0a791806e056f217cacb7f32e7802a551b2f66f5
SkillsContinua <you@skillscontinua.com>
2026-09-16 23:42:34 +0100

    Added Language1/phonemes - 43 Letters Thesis, separated Ndebe vs Nsibidi vs Thesis

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('subjects.urls')), # <-- EMPTY '', not 'subjects/'
+    path('lang1/', include('lang1.urls')),  # advanced 49 phonemes
+    path('', include('subjects.urls')),  # 21 subjects
 ]
