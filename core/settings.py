915aee924d60cd9afcc671c9f2981378af11dce3
SkillsContinua <you@skillscontinua.com>
2026-09-21 09:05:59 +0100

    EMERGENCY FIX: Remove __pycache__ from repo - caused Something went wrong - fix admin + WhiteNoise simple

--- core/settings.py
+++ core/settings.py
@@ -91,4 +91,3 @@ CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000', 'https
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
