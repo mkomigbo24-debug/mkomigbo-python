718b40c6d56a261b63c7843182e7d197614b46cd
SkillsContinua <you@skillscontinua.com>
2026-09-21 12:59:55 +0100

    FIX 404: Add /amuzhi/2026/ year + /awag/tides/ farming regions urls + hide admin big block Amuzhi months

--- amuzhi_calendar/admin.py
+++ amuzhi_calendar/admin.py
@@ -1,20 +1,8 @@
 from django.contrib import admin
-from .models import AmuzhiYear, AmuzhiMonth, MarketDay, AmuzhiTide
-
-@admin.register(AmuzhiYear)
-class AmuzhiYearAdmin(admin.ModelAdmin):
-    list_display = ['gregorian_year', 'igbo_year', 'is_leap', 'year_start_gregorian', 'anchor_market_day']
-
-@admin.register(AmuzhiMonth)
-class AmuzhiMonthAdmin(admin.ModelAdmin):
-    list_display = ['year', 'number', 'name_igbo', 'days_in_month', 'gregorian_start']
-
-@admin.register(MarketDay)
-class MarketDayAdmin(admin.ModelAdmin):
-    list_display = ['date_gregorian', 'market_day', 'igbo_day', 'month', 'moon_symbol', 'illumination']
-    list_filter = ['market_day', 'month__year']
-    search_fields = ['date_gregorian']
-
-@admin.register(AmuzhiTide)
-class AmuzhiTideAdmin(admin.ModelAdmin):
-    list_display = ['date', 'location', 'high_tide', 'low_tide', 'height_m']
+# HIDDEN - Remove big block not needed - uncomment if you need admin for Amuzhi
+# from .models import AmuzhiMonth, AmuzhiTide, AmuzhiYear, MarketDay
+# admin.site.register(AmuzhiMonth)
+# admin.site.register(AmuzhiTide)
+# admin.site.register(AmuzhiYear)
+# admin.site.register(MarketDay)
+# All Amuzhi now public via /amuzhi/ - no admin block needed
