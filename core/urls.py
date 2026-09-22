56525d59c0a165956609a5e2c67994e3ce83c33c
mkomigbo24User <mkomigbo24@gmail.com>
2026-09-22 05:07:30 +0000

    FIX static serve - add static() helper for manifest + sw - 0 silenced

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('amuzhi/', include('amuzhi_calendar.urls')), # PUBLIC - easily accessible
     path('awag/', include('africa_weekly.urls')), # PUBLIC - /awag/ -> africa_weekly app - no conflict!
     path('', include('subjects.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
