4fbb8a6c503e60fdad69c61daa4f7bf2ddf5e6e2
mkomigbo24User <mkomigbo24@gmail.com>
2026-09-22 01:22:46 +0000

    STEP D: PWA offline - manifest + sw + icons - installable

---
+++ static/sw.js
@@ -0,0 +1,4 @@
+const CACHE='mkomigbo-v1';
+const ASSETS=['/amuzhi/','/amuzhi/calculator/','/amuzhi/quiz/','/amuzhi/festival/','/static/manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
