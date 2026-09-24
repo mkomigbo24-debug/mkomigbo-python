4d46aad11246987c325b2d9a00f7f04e59a862e3
SkillsContinua <you@skillscontinua.com>
2026-09-24 09:30:08 +0100

    FIX AWAG 404 - add awag/ to core/urls.py - A) 200 OK

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('subjects/', include('subjects.urls')),
     path('amuzhi/', include('amuzhi_calendar.urls')),
+    path('awag/', include('africa_weekly.urls')),  # <-- ADD THIS! FIX 404!
     path('lang1/', include('lang1.urls')),
     path('', views.home, name='home'),
 ]
