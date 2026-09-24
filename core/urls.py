a9f650c61dbd5382a6f380a67cdeed225fb92b6f
SkillsContinua <you@skillscontinua.com>
2026-09-24 10:38:19 +0100

    FIX static serving in DEBUG - audio 200 OK

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,8 @@ urlpatterns = [
     path('lang1/', include('lang1.urls')),
     path('', views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    if hasattr(settings, 'MEDIA_URL'):
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
