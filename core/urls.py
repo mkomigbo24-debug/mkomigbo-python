8dea41cf553532d586d767bf2ab9d70a9a2e6460
SkillsContinua <you@skillscontinua.com>
2026-09-25 03:03:10 +0100

    ADD SIGNUP: /accounts/signup/ for first time users

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from .views_sitemap import custom_sitemap
+from . import views_sitemap
+from . import views_auth  # ADD THIS
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/signup/', views_auth.signup, name='signup'),  # ADD THIS - Before auth.urls
     path('accounts/', include('django.contrib.auth.urls')),
     path('subjects/', include('subjects.urls')),
-    path('amuzhi/', include('amuzhi_calendar.urls')),
-    path('awag/', include('africa_weekly.urls')),
+    path('amuzhi/', include('amuzhi.urls')),
+    path('awag/', include('awag.urls')),
     path('lang1/', include('lang1.urls')),
     path('community/', include('community.urls')),
-    path('sitemap.xml', custom_sitemap, name='django-sitemap'),
-    path('', views.home, name='home'),
+    path('sitemap.xml', views_sitemap.sitemap_view, name='django-sitemap'),
+    path('', views_sitemap.home, name='home'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
